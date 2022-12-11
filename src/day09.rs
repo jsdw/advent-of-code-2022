a94227d49ab1c06697ae5e8e08eeb34273f021cf
James Wilson <james@jsdw.me>
2022-12-11 00:45:01 +0000

    fix up day9-2

--- src/day09.rs
+++ src/day09.rs
@@ -21,7 +21,7 @@ pub fn star1(file: File) -> Result<usize, anyhow::Error> {
 pub fn star2(file: File) -> Result<usize, anyhow::Error> {
     let mut seen_tail_pos = HashSet::<(i32,i32)>::from_iter([(0,0)]);
     let mut head_pos = (0,0);
-    let tails = vec![Cell::new((0,0)); 10];
+    let tails = vec![Cell::new((0,0)); 9];
     for (dir, count) in parse_input(&file.contents) {
         for _ in 0..count {
             head_pos = dir.nudge(head_pos);
@@ -37,6 +37,7 @@ pub fn star2(file: File) -> Result<usize, anyhow::Error> {
     Ok(seen_tail_pos.len())
 }
 
+// This could be much less verbose, but oh well.
 fn move_tail((tx,ty): (i32,i32), (hx,hy): (i32,i32)) -> (i32,i32) {
     // same; leave
     if hx == tx && hy == ty { (tx,ty) }
@@ -51,7 +52,15 @@ fn move_tail((tx,ty): (i32,i32), (hx,hy): (i32,i32)) -> (i32,i32) {
     else if tx == hx { (tx, pull_1d(ty, hy)) }
     else if ty == hy { (pull_1d(tx, hx), ty) }
 
-    // move for further diagonals
+    // handle "2-away" diagonals with a diagonal move. This
+    // only happens when lots of tails, because while the head
+    // can never move diagonally, any tail can.
+    else if hx-2 == tx && hy-2 == ty { (hx-1,hy-1) }
+    else if hx-2 == tx && hy+2 == ty { (hx-1,hy+1) }
+    else if hx+2 == tx && hy+2 == ty { (hx+1,hy+1) }
+    else if hx+2 == tx && hy-2 == ty { (hx+1,hy-1) }
+
+    // move to the side of the head for further diagonals
     else if hx-1 == tx { (hx, pull_1d(ty, hy)) }
     else if hx+1 == tx { (hx, pull_1d(ty, hy)) }
     else if hy-1 == ty { (pull_1d(tx, hx), hy) }
